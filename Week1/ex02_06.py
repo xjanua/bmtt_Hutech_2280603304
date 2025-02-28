b30097dac468de5cfba751bfdd4de289b558692d
xjanua <ktoanhutech@gmail.com>
2025-02-28 15:08:48 +0700

    add

---
+++ Week1/ex02_06.py
@@ -0,0 +1,20 @@
+
+input_str = input("Nhập X, Y: ")
+
+
+dimensions = [int(x) for x in input_str.split(',')]
+
+
+rowNum = dimensions[0]
+colNum = dimensions[1]
+
+
+multiList = [[0 for col in range(colNum)] for row in range(rowNum)]
+
+
+for row in range(rowNum):
+    for col in range(colNum):
+        multiList[row][col] = row * col
+
+
+print(multiList)
---
+++ Week1/ex02_08.py
@@ -0,0 +1,20 @@
+
+def chia_het_cho_5(so_nhi_phan):
+
+    so_thap_phan = int(so_nhi_phan, 2)
+
+    return so_thap_phan % 5 == 0
+
+
+chuoi_so_nhi_phan = input("Nhập chuỗi số nhị phân (phân tách bởi dấu phẩy): ")
+
+
+so_nhi_phan_list = chuoi_so_nhi_phan.split(',')
+so_chia_het_cho_5 = [so for so in so_nhi_phan_list if chia_het_cho_5(so)]
+
+
+if so_chia_het_cho_5:
+    ket_qua = ','.join(so_chia_het_cho_5)
+    print("Các số nhị phân chia hết cho 5 là:", ket_qua)
+else:
+    print("Không có số nhị phân nào chia hết cho 5 trong chuỗi đã nhập.")
